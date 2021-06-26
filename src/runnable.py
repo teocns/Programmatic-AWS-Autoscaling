befd523869387114dbccd3013c91485612f1c44d
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-3.compute.internal>
2021-06-26 19:05:50 +0000

    E

--- src/runnable.py
+++ src/runnable.py
@@ -1,6 +1,6 @@
 import time
-from src.lambda_function import lambda_handler
+from lambda_function import lambda_handler
 while True:
-    time.sleep(10)    
     lambda_handler(None,None)
+    time.sleep(10)    
     
